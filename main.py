63fdc68b176b287627887159f255bfa85d0735d1
dcook <daniel.j.cook.28@gmail.com>
2025-06-01 19:24:44 -0500

    added labels to plotly charts

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ dates, pos_score, neg_score = get_diary_sentiment()
 
 st.title("Diary Tone")
 st.subheader("Positivity")
-fig = px.line(x=dates, y=pos_score)
+fig = px.line(x=dates, y=pos_score, labels={"x": "Date", "y": "Positivity"})
 st.plotly_chart(fig)
 
 st.subheader("Negativity")
-fig = px.line(x=dates, y=neg_score)
+fig = px.line(x=dates, y=neg_score, labels={"x": "Date", "y": "Negativity"})
 st.plotly_chart(fig)
